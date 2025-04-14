376af54242c04cfe22dcfede4dec35e6fc0b5552
andrzejs <andrzejs@pc-andrzejs>
2025-04-14 20:57:28 +0200

    Add version.ts file with version and name constants

---
+++ src/common/version.ts
@@ -0,0 +1,13 @@
+/**
+ * Version and name information for the MCP City provider
+ */
+
+/**
+ * The current version of the MCP City provider
+ */
+export const VERSION = "0.0.1";
+
+/**
+ * The name of the MCP City provider
+ */
+export const NAME = "mcpcity-sindbad";
